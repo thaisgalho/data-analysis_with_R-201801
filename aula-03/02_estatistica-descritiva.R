cac80e7ce8dffce80d354401010e516e61ecc3c6
cris_weber <cris_weber@bitbucket.org>
2018-05-14 12:53:52 -0300

    ajuste locale/encoding aula 03

--- aula-03/02_estatistica-descritiva.R
+++ aula-03/02_estatistica-descritiva.R
@@ -143,11 +143,25 @@ subset_salarios %>%
 print("Atividade")
 ## Modificar o Dataset para criação de nova variável
 
+subset_com_ano <- subset_salarios %>%
+  mutate(ano_ingresso = year(DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO)) 
+
 ## Determine o tempo médio de trabalho em anos, em nível nacional
+subset_com_ano %>%
+  summarise(tempo_medio = mean(year(today()) - ano_ingresso))
 
 ## Determine o tempo médio de trabalho em anos, por UF
+subset_com_ano %>%
+  group_by(UF_EXERCICIO) %>%
+  summarise(tempo_medio = mean(year(today()) - ano_ingresso)) %>%
+  arrange(desc(tempo_medio)) %>% View()
 
 ## Determine a média salarial por ano de ingresso
+subset_com_ano %>%
+  group_by(ano_ingresso) %>%
+  summarise(media_salarial = mean(REMUNERACAO_REAIS)) %>%
+  arrange(desc(media_salarial))
+
 
 
 #' >> FIM DA ATIVIDADE
