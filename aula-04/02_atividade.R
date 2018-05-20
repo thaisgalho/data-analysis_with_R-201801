f2c053d1fe06e2066d9a7dbb822366479843ceba
unknown <you@example.com>
2018-05-20 18:50:07 -0300

    atividades aula 4

--- aula-04/02_atividade.R
+++ aula-04/02_atividade.R
@@ -12,8 +12,11 @@ head(salarios,20)
 salarios%>%
   mutate(REMUNERACAO_FINAL= (REMUNERACAO_REAIS + (REMUNERACAO_DOLARES * 3.2421)))%>%
   filter(REMUNERACAO_FINAL>900)%>%
-  select(ID_SERVIDOR_PORTAL, REMUNERACAO_REAIS, REMUNERACAO_DOLARES, REMUNERACAO_FINAL)
-  
+  select(ID_SERVIDOR_PORTAL, REMUNERACAO_REAIS, REMUNERACAO_DOLARES, REMUNERACAO_FINAL,DATA_INGRESSO_ORGAO,DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO,DESCRICAO_CARGO,ORGSUP_LOTACAO,ORGSUP_EXERCICIO)->
+  subset_salarios
+
+subset_salarios%>%
+  head(20)
 ### 1 ####
 ## 
 ## Correlação de ano de ingresso por cargo
@@ -25,7 +28,25 @@ salarios%>%
 ## 
 ### # ####
 
-### 2 ###
+subset_salarios %>%
+  group_by(DESCRICAO_CARGO) %>%
+  summarise( SERVIDORES = n(),CORRELACAO = cor(x = ( 2018 - year (DATA_INGRESSO_ORGAO)), y = (2018 -year(DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO))))%>%
+  ungroup()%>%
+  filter(SERVIDORES >=200)%>%
+  arrange(SERVIDORES)%>%
+  mutate(DIRECAO = (ifelse(CORRELACAO>0,'POSITIVA','NEGATIVA')))%>%
+  mutate(CORRELACAO_ABSOLUTA = (ifelse(CORRELACAO>0,CORRELACAO,(CORRELACAO * (-1)))))%>%
+  mutate(FORCA = ifelse(CORRELACAO_ABSOLUTA >=0.9, 'MUITO FORTE',
+                 ifelse(CORRELACAO_ABSOLUTA >= 0.7 & CORRELACAO_ABSOLUTA < 0.9, 'FORTE',    
+                 ifelse(CORRELACAO_ABSOLUTA >= 0.5 & CORRELACAO_ABSOLUTA < 0.7, 'MODERADA',
+                 ifelse(CORRELACAO_ABSOLUTA >= 0.3 & CORRELACAO_ABSOLUTA < 0.5, 'FRACA','DESPREZÍVEL')))))%>%
+  select(DESCRICAO_CARGO, CORRELACAO, DIRECAO, FORCA, CORRELACAO_ABSOLUTA) -> atividade1
+
+atividade1%>%
+  select(DESCRICAO_CARGO, CORRELACAO, DIRECAO, FORCA)
+  
+  
+    ### 2 ###
 ##
 ## - A partir do dataset do exercício anterior, selecione os 10 cargos de correlação mais forte (seja positiva ou negativa) e os 
 ##   10 cargos de correlação mais fraca (de novo, independente de ser positiva ou negativa)
@@ -35,3 +56,47 @@ salarios%>%
 ##
 ### # ###
 
+atividade1%>%
+  arrange(CORRELACAO_ABSOLUTA)%>%
+  head(10)%>%
+  pull(DESCRICAO_CARGO) -> cargos
+
+subset_salarios %>%
+    filter(DESCRICAO_CARGO %in% cargos) %>%
+    count(ORGSUP_LOTACAO) %>%
+    arrange(desc(n)) %>%
+    head(1)%>% 
+    pull(ORGSUP_LOTACAO) -> moda_orgsup_lotacaof
+
+subset_salarios %>%
+    count(ORGSUP_EXERCICIO) %>%
+    arrange(desc(n)) %>%
+    head(1)%>% 
+    pull(ORGSUP_EXERCICIO) -> moda_orgsup_exerciciof
+
+atividade1%>%
+  arrange(CORRELACAO_ABSOLUTA)%>%
+  tail(10)%>%
+  pull(DESCRICAO_CARGO) -> cargos
+
+subset_salarios %>%
+  filter(DESCRICAO_CARGO %in% cargos) %>%
+  count(ORGSUP_LOTACAO) %>%
+  arrange(desc(n)) %>%
+  head(1)%>% 
+  pull(ORGSUP_LOTACAO) -> moda_orgsup_lotacao
+
+subset_salarios %>%
+  count(ORGSUP_EXERCICIO) %>%
+  arrange(desc(n)) %>%
+  head(1)%>% 
+  pull(ORGSUP_EXERCICIO) -> moda_orgsup_exercicio
+
+print('Dos 10 cargos correlacao mais forte')
+print(paste('Orgao Lotacao:',moda_orgsup_lotacao,'   Orgao Exercicio:',moda_orgsup_exercicio))
+print('Dos 10 cargos correlacao mais fracas')
+print(paste('Orgao Lotacao:',moda_orgsup_lotacaof,'   Orgao Exercicio:',moda_orgsup_exerciciof))
+
+##Comentario
+## a diferenca das modas  entre orgao de lotacao e de exercio e o exato oposto entre os cargos de 
+## correlação mais forte e mais fraca
